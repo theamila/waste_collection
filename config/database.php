8d476ddfd591729af85746bcd35151342afda544
theamila <84174433+theamila@users.noreply.github.com>
2025-02-22 17:27:26 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection
-$conn = new mysqli('localhost', 'root', '', 'waste_collection');
+$conn = new mysqli('osi-ltsd-svr.mysql.database.azure.com', 'zbzqbabxgz', 'Ystp$1NYCd4nL5te', 'waste');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
