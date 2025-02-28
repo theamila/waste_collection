e5186970873714a1021fedd912322ef72b8a9b1e
theamila <84174433+theamila@users.noreply.github.com>
2025-02-28 12:24:39 +0530

    Update area.php

--- manager/area.php
+++ manager/area.php
@@ -64,10 +64,10 @@ elseif ($action === 'send') { // Correctly aligned with other actions
 	if ($result->num_rows > 0) {
     		// Iterate through the fetched entries
     		while($row = $result->fetch_assoc()) {
-       		echo "ID: " . $row["lane_no"]. " - Name: " . $row["house_no"]. " - Mobile: " . $row["mobile_number"]. "<br>";
+       		//echo "ID: " . $row["lane_no"]. " - Name: " . $row["house_no"]. " - Mobile: " . $row["mobile_number"]. "<br>";
 		$url = 'https://app.notify.lk/api/v1/send?user_id=29099&api_key=04Q0g5ASxkJa6Y4IAWmf&sender_id=NotifyDEMO&to='. $row["mobile_number"].'&message=Waste Collection Today. Lane'. $row["lane_no"];
 		//$data = file_get_contents($url);
-			echo $url;
+			echo "Message Sent";
     		}
 	} else {
     		echo "0 results";
